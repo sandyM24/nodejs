e073c03d40f74f8eb685c7fbd098ba5f425d0a7d
sandyM24 <iambansalsandeep@outlook.com>
2018-12-25 20:17:21 -0800

    How to create module and load a module.

--- node_demo/index.js
+++ node_demo/index.js
@@ -1,1 +1,13 @@
-console.log('Hello NodeJS');
+// What is NodeJS?
+// NodeJs is an open source, cross platform JavaScript runtime enviornment that executes JavaScript code outside of a browser.
+
+// In node we dont't have 'window' object rather we have 'global' object.
+
+// Like in browser javascript variables and functions declare with var are added to window object, but in case of node variables and functions declare with var keyword does not added to global object because node use module system.
+
+// Every file in node application is called a module and variables and functions define in that file is only scoped to only that file, if we want to use variables and functions outside the file we need to exports it.
+
+const logger = require('./logger'); // require() return a exports.
+
+console.log(module); // Module is an object that contains bunch of properties including a exports.
+logger('This is message');
